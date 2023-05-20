1b43500752c17c13c489bf9ddb4190b0cc8dcb31
kamiakhs <admin@kamiak.org>
2023-05-20 15:50:10 -0700

    Very basic routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,64 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { Route, Link, Routes, useNavigate } from 'react-router-dom'
+
+function SignInPage({onPasswordChange, onSubmit}) {
+  return (
+    <div className='w-full max-w-md space-y-8'>
+      <div>
+        <h2 className='text-center text-3xl font-bold tracking-tight text-slate-900'>Sign in to RGB Manager</h2>
+        <p className='mt-2 text-center text-sm text-slate-600'>Verify your identity to continue</p>
+      </div>       
+      <form className="mt-8 space-y-6" onSubmit={(e) => {
+        e.preventDefault();
+        onSubmit();
+      }}>
+        <div className="-space-y-px rounded-md shadow-sm">
+          <div>
+            <label htmlFor="password" className="sr-only">Admin password</label>
+            <input onChange={e => onPasswordChange(e.target.value)} id="password" name="password" type="password" autoComplete="off" className="relative block w-full rounded-md border-0 py-1.5 text-slate-900 ring-1 ring-inset ring-slate-300 placeholder:text-slate-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 px-3" placeholder="Admin password (hint: happy day)"/>
+          </div>
+        </div>
+        <div>
+          <button className="w-full rounded-md py-2 px-3 text-center text-sm font-semibold text-white bg-indigo-600 hover:bg-indigo-500 focus-visible:outline outline-2 outline-offset-2 outline-indigo-600">
+            Log in
+          </button>
+        </div>
+      </form>
+    </div>
+  )
+}
+
+function AdminRoute() {
+  const [password, setPassword] = useState('');
+  return (
+    <SignInPage
+      onPasswordChange={setPassword}
+      onSubmit={async()=>{
+        console.log('password submitted: '+password);
+      }}
+    />
+  )
+}
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='flex flex-col min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8 bg-slate-100'>
+      <Routes>
+        <Route path='/' element={
+          <div className='w-full max-w-md space-y-8'>
+            <div>
+              <h2 className='text-center text-3xl font-bold tracking-tight text-slate-900'>Random Gift Box</h2>
+              <p className='mt-2 text-center text-sm text-slate-600'>click to open</p>
+            </div>        
+            <button className='w-full rounded-md py-2 px-3 text-center text-sm font-semibold text-white bg-indigo-600 hover:bg-indigo-500 focus-visible:outline outline-2 outline-offset-2 outline-indigo-600'>
+              Open
+            </button>
+          </div>
+        }/>
+        <Route path='/admin' element={<AdminRoute/>}/>
+      </Routes>
+    </div>
   )
 }
 
